1217984468a9fe6e232f167b3b7ab87ccc156ebe
Flor <florangelsalgado20@gmail.com>
2026-05-16 14:06:41 -0400

    Update Program.cs

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,22 +1,18 @@
 using System;
 
-namespace game
+namespace Exercise1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // 1. Prompt the user for input
             Console.Write("What is your first name? ");
             string firstName = Console.ReadLine();
 
             Console.Write("What is your last name? ");
             string lastName = Console.ReadLine();
 
-            // 2. Add a blank line for clean formatting
             Console.WriteLine();
-
-            // 3. Output the result using string interpolation ($"")
             Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
         }
     }
