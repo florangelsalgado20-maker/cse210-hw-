531f89595cc995138c1717ba48a002202d84d6c6
florangelsalgado20-maker <florangelsalgado20@gmail.com>
2026-05-15 20:19:56 -0400

    W01 Exercise 2 completed

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,74 @@
 using System;
 
-class Program
+namespace game
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        static void Main(string[] args)
+        {
+            Console.Write("What is your grade percentage? ");
+            string userInput = Console.ReadLine();
+            int percent = int.Parse(userInput);
+
+            string letter = "";
+            string sign = "";
+
+            if (percent >= 90)
+            {
+                letter = "A";
+            }
+            else if (percent >= 80)
+            {
+                letter = "B";
+            }
+            else if (percent >= 70)
+            {
+                letter = "C";
+            }
+            else if (percent >= 60)
+            {
+                letter = "D";
+            }
+            else
+            {
+                letter = "F";
+            }
+
+            int lastDigit = percent % 10;
+
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+            else
+            {
+                sign = "";
+            }
+
+            if (letter == "A" && sign == "+")
+            {
+                sign = "";
+            }
+
+            if (letter == "F")
+            {
+                sign = "";
+            }
+
+            Console.WriteLine($"Your letter grade is: {letter}{sign}");
+
+            if (percent >= 70)
+            {
+                Console.WriteLine("Congratulations! You passed the course.");
+            }
+            else
+            {
+                Console.WriteLine("Don't give up! Keep trying for next time.");
+            }
+        }
     }
 }
