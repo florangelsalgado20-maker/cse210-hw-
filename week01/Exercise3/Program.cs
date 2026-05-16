e1d53f25aa0fe0ecc4000baa92b247229c082e67
florangelsalgado20-maker <florangelsalgado20@gmail.com>
2026-05-15 20:27:13 -0400

    Finished Exercise 3 - Loops

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,46 @@
 using System;
 
-class Program
+namespace game
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        static void Main(string[] args)
+        {
+            string playAgain = "yes";
+
+            while (playAgain == "yes")
+            {
+                Random randomGenerator = new Random();
+                int magicNumber = randomGenerator.Next(1, 101);
+
+                int guess = -1;
+                int guessCount = 0;
+
+                while (guess != magicNumber)
+                {
+                    Console.Write("What is your guess? ");
+                    string userInput = Console.ReadLine();
+                    guess = int.Parse(userInput);
+                    guessCount++;
+
+                    if (guess < magicNumber)
+                    {
+                        Console.WriteLine("Higher");
+                    }
+                    else if (guess > magicNumber)
+                    {
+                        Console.WriteLine("Lower");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You guessed it!");
+                        Console.WriteLine($"It took you {guessCount} guesses.");
+                    }
+                }
+
+                Console.Write("Do you want to play again (yes/no)? ");
+                playAgain = Console.ReadLine().ToLower();
+            }
+        }
     }
 }
