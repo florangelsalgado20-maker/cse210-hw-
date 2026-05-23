8fa489d9ffea4a93cd4326fe2f45617104c07b5a
Flor <florangelsalgado20@gmail.com>
2026-05-23 18:17:17 -0400

    Update Journal.cs

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -1,1 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
 
+public class Journal
+{
+    private List<Entry> _entries = new List<Entry>();
+
+    public void AddEntry(Entry entry)
+    {
+        _entries.Add(entry);
+    }
+
+    public void DisplayJournal()
+    {
+        foreach (Entry entry in _entries)
+        {
+            entry.Display();
+        }
+    }
+
+    public void SaveToFile(string filename)
+    {
+        using (StreamWriter writer = new StreamWriter(filename))
+        {
+            foreach (Entry entry in _entries)
+            {
+                writer.WriteLine(entry.ToFileString());
+            }
+        }
+    }
+
+    public void LoadFromFile(string filename)
+    {
+        _entries.Clear();
+        using (StreamReader reader = new StreamReader(filename))
+        {
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                string[] parts = line.Split(new string[] { "~|~" }, StringSplitOptions.None);
+                if (parts.Length == 3)
+                {
+                    Entry entry = new Entry(parts[1], parts[2], parts[0]);
+                    _entries.Add(entry);
+                }
+            }
+        }
+    }
+}
