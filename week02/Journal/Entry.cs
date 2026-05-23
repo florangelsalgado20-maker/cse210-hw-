220c016d2ae42e1be8a794246b398f2586d4b37d
Flor <florangelsalgado20@gmail.com>
2026-05-23 18:16:50 -0400

    Update Entry.cs

--- week02/Journal/Entry.cs
+++ week02/Journal/Entry.cs
@@ -1,1 +1,27 @@
+using System;
 
+public class Entry
+{
+    public string Prompt { get; set; }
+    public string Response { get; set; }
+    public string Date { get; set; }
+
+    public Entry(string prompt, string response, string date)
+    {
+        Prompt = prompt;
+        Response = response;
+        Date = date;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"{Date} - {Prompt}");
+        Console.WriteLine(Response);
+        Console.WriteLine();
+    }
+
+    public string ToFileString()
+    {
+        return $"{Date}~|~{Prompt}~|~{Response}";
+    }
+}
