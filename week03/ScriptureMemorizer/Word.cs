d576022009d186c75e040888390dad8802619cb3
Flor <florangelsalgado20@gmail.com>
2026-05-27 12:19:18 -0400

    Update Word.cs

--- week03/ScriptureMemorizer/Word.cs
+++ week03/ScriptureMemorizer/Word.cs
@@ -1,1 +1,24 @@
+using System;
 
+namespace ScriptureMemorizer
+{
+    public class Word
+    {
+        private string _text;
+        private bool _isHidden;
+
+        public Word(string text)
+        {
+            _text = text;
+            _isHidden = false;
+        }
+
+        public void Hide() => _isHidden = true;
+        public bool IsHidden => _isHidden;
+
+        public override string ToString()
+        {
+            return _isHidden ? new string('_', _text.Length) : _text;
+        }
+    }
+}
