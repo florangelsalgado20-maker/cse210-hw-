825dfc1c6e59fd8f92554a14ee61d429b0c1d919
Flor <florangelsalgado20@gmail.com>
2026-05-27 12:19:34 -0400

    Update Reference.cs

--- week03/ScriptureMemorizer/Reference.cs
+++ week03/ScriptureMemorizer/Reference.cs
@@ -1,1 +1,35 @@
+using System;
 
+namespace ScriptureMemorizer
+{
+    public class Reference
+    {
+        private string _book;
+        private int _chapter;
+        private int _startVerse;
+        private int? _endVerse;
+
+        public Reference(string book, int chapter, int verse)
+        {
+            _book = book;
+            _chapter = chapter;
+            _startVerse = verse;
+            _endVerse = null;
+        }
+
+        public Reference(string book, int chapter, int startVerse, int endVerse)
+        {
+            _book = book;
+            _chapter = chapter;
+            _startVerse = startVerse;
+            _endVerse = endVerse;
+        }
+
+        public override string ToString()
+        {
+            return _endVerse.HasValue 
+                ? $"{_book} {_chapter}:{_startVerse}-{_endVerse.Value}" 
+                : $"{_book} {_chapter}:{_startVerse}";
+        }
+    }
+}
